4db62c2c5deeb29b017f998a4e4302fc25936ead
Moises Terrones <moises.tersal@gmail.com>
2019-11-01 15:02:16 -0600

    Add a part of the database implementation.

--- app/src/main/java/com/example/musiclibrary/database/MusicDatabase.kt
+++ app/src/main/java/com/example/musiclibrary/database/MusicDatabase.kt
@@ -1,9 +1,37 @@
 package com.example.musiclibrary.database
 
+import android.content.Context
 import androidx.room.Database
+import androidx.room.Room
 import androidx.room.RoomDatabase
 
 @Database(entities = [MusicRecord::class], version = 1, exportSchema = false)
 abstract class MusicDatabase : RoomDatabase() {
+
+    abstract val musicDatabaseDao : MusicDatabaseDao
+
+    companion object {
+
+        @Volatile
+        private var INSTANCE : MusicDatabase? = null
+
+        fun getInstance(context: Context) : MusicDatabase {
+            synchronized(this) {
+                var instance = INSTANCE
+
+                if(instance == null) {
+                    instance = Room.databaseBuilder(
+                        context.applicationContext,
+                        MusicDatabase::class.java,
+                        "music_history_database"
+                    ).fallbackToDestructiveMigration().build()
+
+                    INSTANCE = instance
+                }
+
+                return instance
+            }
+        }
+    }
 
 }
