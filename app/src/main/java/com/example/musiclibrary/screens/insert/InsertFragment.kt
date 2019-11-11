c7c729eb0e6e5981b36f41f72b65f15979a114d9
Moises Terrones <moises.tersal@gmail.com>
2019-11-11 16:10:12 -0600

    Add implementation in the fragment to create references to the ViewModel

--- app/src/main/java/com/example/musiclibrary/screens/insert/InsertFragment.kt
+++ app/src/main/java/com/example/musiclibrary/screens/insert/InsertFragment.kt
@@ -6,10 +6,16 @@ import android.view.View
 import android.view.ViewGroup
 import androidx.databinding.DataBindingUtil
 import androidx.fragment.app.Fragment
+import androidx.lifecycle.ViewModelProviders
 import com.example.musiclibrary.R
+import com.example.musiclibrary.database.MusicDatabase
 import com.example.musiclibrary.databinding.FragmentInsertBinding
 
 class InsertFragment: Fragment() {
+
+    private lateinit var viewModel: InsertViewModel
+    private lateinit var viewModelFactory: InsertViewModelFactory
+
     override fun onCreateView(
         inflater: LayoutInflater,
         container: ViewGroup?,
@@ -18,6 +24,19 @@ class InsertFragment: Fragment() {
 
         val binding: FragmentInsertBinding = DataBindingUtil.inflate(
             inflater, R.layout.fragment_insert, container, false)
+
+        // Application context
+        val application = requireNotNull(this.activity).application
+
+        // Reference to the DAO of the database
+        val dataSource = MusicDatabase.getInstance(application).musicDatabaseDao
+
+        // Get the factory
+        viewModelFactory = InsertViewModelFactory(dataSource, application)
+
+        // Reference to the viewModel
+        viewModel = ViewModelProviders.of(this, viewModelFactory)
+            .get(InsertViewModel::class.java)
 
         return binding.root
     }
